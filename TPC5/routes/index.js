879e23188c3db62062aa86f14a242ae60e401c15
Falape <filipe_s_fernandes@hotmail.com>
2022-03-28 16:07:47 +0100

    Falta extras

--- TPC5/routes/index.js
+++ TPC5/routes/index.js
@@ -26,36 +26,45 @@ router.get('/musicas', function (req, res, next) {
     })
 });
 
-router.get('/musicas/:id', function (req, res, next) {
-  axios.get("http://localhost:3000/musicas/id")
-    .then(response => {
-      var lista = response.data
-      res.render('musica', { musica: lista });
-    })
-    .catch(function (erro) {
-      res.render('error', { error: erro });
-    })
-});
-
 router.get('/musicas/prov', function (req, res, next) {
   axios.get("http://localhost:3000/musicas")
     .then(response => {
-      var lista = response.data
-      res.render('provincias', { provincias: lista });
+      var data = response.data.map(d => d.prov);
+      let cleanData = [];
+      
+      data.forEach((word,index) => {
+        if(data.indexOf(word) == index){
+          cleanData.push(word)
+        }
+      });
+      res.render('provincias', { provincias: cleanData.sort() });
     })
     .catch(function (erro) {
       res.render('error', { error: erro });
     })
 });
 
-router.get('/musicas/prov/:nome', function (req, res, next) {
-  axios.get("http://localhost:3000/musicas?prov=nome")
+router.get('/musicas/:id', function (req, res, next) {
+  axios.get("http://localhost:3000/musicas/"+req.params.id)
     .then(response => {
-      var lista = response.data
-      res.render('provincia', { provincia: lista });
+      var data = response.data
+      res.render('musica', {id: req.params.id, musica: data });
     })
     .catch(function (erro) {
       res.render('error', { error: erro });
     })
 });
+
+
+router.get('/musicas/prov/:prov', function (req, res, next) {
+  axios.get("http://localhost:3000/musicas?prov="+req.params.prov)
+    .then(response => {
+      var lista = response.data
+      res.render('provincia', {prov: req.params.prov ,provincia: lista });
+    })
+    .catch(function (erro) {
+      res.render('error', { error: erro });
+    })
+});
+
 module.exports = router;
